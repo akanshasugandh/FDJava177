964bf620db08e684038c40684bcbcc268e838b0d
akanshasugandh <akanshasugandh211@gmail.com>
2024-07-10 03:46:30 +0530

    Add m1 method
    Add m1 method to get employee data

--- MicroService-GitHubDemo/src/main/java/com/cjc/main/MicroServiceGitHubDemoApplication.java
+++ MicroService-GitHubDemo/src/main/java/com/cjc/main/MicroServiceGitHubDemoApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MicroServiceGitHubDemoApplication {
 
+	public void m1()
+	{
+		System.out.println("m1 ---> method");
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(MicroServiceGitHubDemoApplication.class, args);
 	}
